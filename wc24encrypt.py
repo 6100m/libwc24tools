2f79fc8114087f2ff87e0fb032c8ab5349e1db3c
6100m <77129415+6100m@users.noreply.github.com>
2021-11-13 20:30:44 -0600

    Many fixes

--- wc24encrypt.py
+++ wc24encrypt.py
@@ -1,50 +1,48 @@
-import rsa
-import struct
+from xcrypt import generate4
+from struct import pack
 from binascii import unhexlify
 from Crypto.Cipher import AES
 from libnlzsstools import compress
-from os import urandom
-
 
 def u8(data):
-    return struct.pack(">B", data)
+    return pack(">B", data)
 
 def u16(data):
-    return struct.pack(">H", data)
+    return pack(">H", data)
 
 def u32(data):
-    return struct.pack(">I", data)
+    return pack(">I", data)
 
 def ParseContainer(type_data, buffer_data, aes_key, iv_key, rsa_key):
     compressed_data = _compress(bytes(buffer_data.read()))
-    private_key = rsa.PrivateKey.load_pkcs1(rsa_key, "PEM")
-    signature = rsa.sign(data, private_key, "SHA-1")
+    signature = rsa.sign(compressed_data, rsa_key.read(), "SHA-1")
     if iv_key is not None:
         try:
-            iv = unhexlify(iv_key)
+            iv = unhexlify(iv_key.read())
         except:
             iv = iv_key.read()
     else:
-        iv = os.urandom(16)
+        iv = generate4() * 4
     try:
         key = unhexlify(aes_key)
     except:
         key = aes_key.read()
     aes = AES.new(key, AES.MODE_OFB, iv=iv_key)
-    processed = aes.encrypt(compressed_data)
-    content_dict = {}
-    content_dict["magic"] = b"WC24" if type_data == "enc" else u32(0)
-    content_dict["version"] = u32(1) if type_data == "enc" else u32(0)
-    content_dict["filler"] = u32(0)
-    content_dict["crypt_type"] = u8(1) if type_data == "enc" else u8(0)
-    content_dict["pad"] = u8(0) * 3
-    content_dict["reserved"] = u8(0) * 32
-    content_dict["iv"] = iv if type_data == "enc" else u8(0) * 16
-    content_dict["signature"] = signature
-    content_dict["data"] = processed
+    processed = aes.encrypt(compressed_data.read())
+    input_dict = [
+        b"WC24",
+        u32(1),
+        u32(0),
+        u8(1),
+        u8(0) * 3,
+        u8(0) * 32,
+        iv,
+        signature,
+        processed
+    ]
     # Thanks https://www.geeksforgeeks.org/python-convert-dictionary-to-concatenated-string/
     output_dict = []
     res = ' '
-    for values in content.values():
+    for values in input_dict:
         res += item + str(output_dict[values])
     return unhexlify(res)
